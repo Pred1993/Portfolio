de3a64b7d11adeb2aae63585eb8bc42b2eb5f3d0
Ivchenko Artem <metrash.a70@gmail.com>
2022-12-23 18:58:59 +0300

    part footer

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import style from './Footer.module.css';
+import styles from './Footer.module.scss';
 import styleContainer from '../common/styles/Container.module.scss';
+import Title from "../common/components/Title/Title";
+import Portfolio from "../common/components/Portfolio/Portfolio";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTelegram} from "@fortawesome/free-brands-svg-icons/faTelegram";
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons/faLinkedin";
+import {faGithub} from "@fortawesome/free-brands-svg-icons/faGithub";
+import {faVk} from "@fortawesome/free-brands-svg-icons/faVk";
+const arrIcons = [faLinkedin, faTelegram, faGithub, faVk]
+
 
 const Footer = () => {
   return (
-    <div className={style.footerPage}>
-      <div className={`${styleContainer.container} ${style.footerContainer}`}>
-        <h2 style={{ background: 'yellow' }}>Artem Ivchenko</h2>
-        <div className={style.squareContainer}>
-          <div className={style.square}></div>
-          <div className={style.square}></div>
-          <div className={style.square}></div>
-          <div className={style.square}></div>
+    <div className={styles.footerPage}>
+      <div className={`${styleContainer.container} ${styles.footerContainer}`}>
+        <Portfolio name={'Website'}/>
+        <Title textPart1={'Iuchanka '} textPart2={'A.'}/>
+        <div className={styles.squareContainer}>
+          {arrIcons.map((el, index) => {
+            return <div className={styles.square}>
+              <FontAwesomeIcon className={styles.icon} icon={el} />
+            </div>
+          })}
         </div>
         <span
           style={{
