10175271f803cc455b4a9bba630cd9f34b551c20
Ivchenko Artem <metrash.a70@gmail.com>
2022-12-24 16:57:12 +0300

    part contacts refactor

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,23 +1,67 @@
 import React from 'react';
-import style from './Contacts.module.scss';
+import styles from './Contacts.module.scss';
 import styleContainer from '../common/styles/Container.module.scss';
 import Portfolio from "../common/components/Portfolio/Portfolio";
 import Title from "../common/components/Title/Title";
+import SuperButton from "../common/components/SuperButton/SuperButton";
+import {useFormik} from "formik";
+import * as Yup from 'yup';
+import SuperInput from "../common/components/SuperInput/SuperInput";
 
 const Contacts = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      message: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid email address').required('Required')
+    }),
+    onSubmit: (values) => {
+      console.log(values)
+    },
+  });
   return (
-    <div className={style.contactsPage}>
-      <div className={`${styleContainer.container} ${style.contactsContainer}`}>
-        <Portfolio name={'Contacts'} />
-        <Title textPart1={'My '} textPart2={'contacts'} />
-        <form className={style.formContainer}>
-          <input type="text" />
-          <input type="text" />
-          <textarea name="" id="" cols={30} rows={10}></textarea>
-          <button type="submit" className={style.submitBtn}>
-            Send
-          </button>
+    <div className={styles.contactsPage}>
+      <div className={`${styleContainer.container} ${styles.contactsContainer}`}>
+        <Portfolio name={'Contacts'}/>
+        <Title textPart1={'My '} textPart2={'contacts'}/>
+        <form onSubmit={formik.handleSubmit} className={styles.formContainer}>
+          <SuperInput className={styles.name} placeholder={'Name'}
+                      {...formik.getFieldProps('name')}
+          />
+          {/*{formik.touched.email && formik.errors.email && (*/}
+          {/*  <div style={{ color: 'red', fontSize: '12px' }}>{formik.errors.email}</div>*/}
+          {/*)}*/}
+          <SuperInput className={styles.email} placeholder={'e-mail'}
+                      {...formik.getFieldProps('email')}
+          />
+          {formik.touched.email && formik.errors.email && (
+            <div style={{color: 'red', fontSize: '12px'}}>{formik.errors.email}</div>
+          )}
+          <textarea className={styles.message} placeholder={'Message'}
+                    {...formik.getFieldProps('message')}
+          />
+          {/*{formik.touched.email && formik.errors.email && (*/}
+          {/*  <div style={{ color: 'red', fontSize: '12px' }}>{formik.errors.email}</div>*/}
+          {/*)}*/}
+          <div className={styles.submitBtn} >
+            <SuperButton style={{'width': '150px'}}
+                         type="submit"
+            >
+              Sign In
+            </SuperButton>
+          </div>
         </form>
+        {/*<form className={style.formContainer}>*/}
+        {/*  <input type="text" />*/}
+        {/*  <input type="text" />*/}
+        {/*  <textarea name="" id="" cols={30} rows={10}></textarea>*/}
+        {/*  <button type="submit" className={style.submitBtn}>*/}
+        {/*    Send*/}
+        {/*  </button>*/}
+        {/*</form>*/}
         {/*<div className={style.linkContainer}><a href="">send*/}
         {/*</a></div>*/}
       </div>
