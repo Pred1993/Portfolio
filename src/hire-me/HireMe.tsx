88a09db5ef93382da8d26556065265d2fa5c87ec
Ivchenko Artem <metrash.a70@gmail.com>
2023-01-09 20:25:55 +0300

    refactor code part 7

--- src/hire-me/HireMe.tsx
+++ src/hire-me/HireMe.tsx
@@ -14,7 +14,7 @@ const HireMe = () => {
       <div className={`${styleContainer.container} ${style.hireMeContainer}`}>
         <Title textPart1={'Available '} textPart2={'for hire'}/>
         <Reveal cascade={true}>
-            <a href='https://drive.google.com/file/d/1YOp2fSwjcqrEhZmB2Samx7d2V_Ybb_16/view?usp=share_link' target='_blank'
+            <a href='https://drive.google.com/file/d/1KzRwuag_z9fWHHB1r7g56SHpse2tlgMJ/view?usp=share_link' target='_blank'
             >
               <SuperButton style={{'marginTop': '20px', 'marginBottom': '20px'}}>
               Download CV
